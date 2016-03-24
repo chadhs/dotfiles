70b447b0db0248c6bfa0c0b05265962c0173777f
Chad Stovern <chad@stovern.me>
2016-03-23 21:14:27 -0500

    vimrc & leiningen profile updates
    - added lein-ancient for dep update checking
    - replaced the depracated vim-powerline with vim-airline

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -2,6 +2,7 @@
                   [venantius/ultra "0.4.1"]
                   [cider/cider-nrepl "0.11.0"]
                   [luminus/lein-template "2.9.9.2"]
+                  [lein-ancient "0.6.8"]
                   ]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [
