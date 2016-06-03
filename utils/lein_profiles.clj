b5e517f971a7827b04027e7eea4b9370ed15bd04
Chad Stovern <chad@stovern.me>
2016-06-03 15:00:39 -0500

    emacs and leiningen update:
    - added keyboard shortcuts for `kill-sexp` and `backward-kill-sexp`
    - bumped some leiningen plugin versions

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,11 +1,11 @@
 {:user {:plugins [
                   [venantius/ultra "0.4.1"]
-                  [cider/cider-nrepl "0.11.0"]
-                  [luminus/lein-template "2.9.9.2"]
-                  [lein-ancient "0.6.8"]
+                  [cider/cider-nrepl "0.12.0"]
+                  ;[luminus/lein-template "2.9.9.2"]
+                  [lein-ancient "0.6.10"]
                   ]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [
-                       [lein-cljfmt "0.4.1"]
+                       [lein-cljfmt "0.5.3"]
                        [acyclic/squiggly-clojure "0.1.5"]
                        ]}}
