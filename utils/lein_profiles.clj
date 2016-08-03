7047b02c52517457d2c122d3b469483b5525ac04
Chad Stovern <chad@stovern.me>
2016-08-03 10:50:27 -0500

    emacs updates + editorconfig setup + lein speedup:
    - created default editorconfig configuration
    - added editorconfig support to emacs
    - fixed opt command h "hide others" for emacs on macOS
    - tested and pruned down lein config to half the speed of lein repl
      and cider repl startup!!!
    - documentation updates in emacs conf

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,11 +1,6 @@
 {:user {:plugins [
-                  [venantius/ultra "0.4.1"]
-                  [cider/cider-nrepl "0.12.0"]
-                  ;[luminus/lein-template "2.9.9.2"]
                   [lein-ancient "0.6.10"]
+                  ;[venantius/ultra "0.4.1"]
                   ]
-        :ultra {:color-scheme :solarized_dark}
-        :dependencies [
-                       [lein-cljfmt "0.5.3"]
-                       [acyclic/squiggly-clojure "0.1.5"]
-                       ]}}
+        ;:ultra {:color-scheme :solarized_dark}
+        }}
