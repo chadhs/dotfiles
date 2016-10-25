972a479775eb36cccba7345a38333040ebc2a716
Chad Stovern <chad@stovern.me>
2016-10-25 17:23:04 -0500

    emacs & lein update:
    - added nightlight to lein profile
    - added vi style end of file tildes
    - only show cider error buffer on demand
    - load web-mode for jinja files
    - don't use electric returns for double quotes
    - some comment cleanups
    - replaced `loop` call with `cl-loop` to fix error on emacs start
    - remove yas from modeline

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [
                   [lein-ancient "0.6.10"]
+                  ;[nightlight/lein-nightlight "1.0.1"]
                   ;[venantius/ultra "0.4.1"]
                   ]
         ;:ultra {:color-scheme :solarized_dark}
