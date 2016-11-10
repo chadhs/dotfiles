7dc106f3d68d35b22227f84a512d73de1e70ff11
Chad Stovern <chad@stovern.me>
2016-11-10 14:19:01 -0600

    leiningen update:
    - removed stuff i'm not using
    - limited repl print length for testing lazy-seqs

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,9 +1,5 @@
-{:user {:plugins [
-                  [lein-ancient "0.6.10"]
+{:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-create-template "0.2.0"]
-                  [lein-pprint "1.1.2"]
-                  ;[nightlight/lein-nightlight "1.0.1"]
-                  ;[venantius/ultra "0.4.1"]
-                  ]
-        ;:ultra {:color-scheme :solarized_dark}
-        }}
+                  [lein-pprint "1.1.2"]]}}
+
+{:repl {:global-vars {*print-length* 100}}}
