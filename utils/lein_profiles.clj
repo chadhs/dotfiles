8cc4086e754533da309e2661e26bdf64bb629ba7
Chad Stovern <chad@stovern.me>
2016-11-10 22:43:03 -0600

    leiningen update:
    - added lazy-seq repl protection

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-create-template "0.2.0"]
-                  [lein-pprint "1.1.2"]]}}
+                  [lein-pprint "1.1.2"]]
 
-{:repl {:global-vars {*print-length* 100}}}
+        :repl-options {:init (set! *print-length* 100)}}}
