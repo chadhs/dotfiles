892daf46e03b21497e81c06db042df80d5e20940
Chad Stovern <chad@stovern.me>
2016-12-28 09:24:43 -0600

    emacs bootstrap update:
    - prevent yes no prompt for following symlinks
    - annotate contents of bootstrap file

--- editors/emacs.el
+++ editors/emacs.el
@@ -1,2 +1,10 @@
-;; (package-initialize)
+;;; emacs config bootstrap
+
+;; prevent emacs from automatically adding a package section to this file
+;(package-initialize)
+
+;; always follow the emacs-config.org symlink without prompting
+(setq vc-follow-symlinks t)
+
+;; emacs-config.org is where the full emacs configuration lives
 (org-babel-load-file "~/.emacs.d/emacs-config.org")
