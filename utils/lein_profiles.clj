e6156830ebe19038371ca5cdf3ad9f1289378ea6
Chad Stovern <chad@stovern.me>
2017-01-05 09:34:11 -0600

    lein update:
    added a profile section for using manual lein repl with emacs/cider in
    a pinch.

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,5 +1,8 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-create-template "0.2.0"]
                   [lein-pprint "1.1.2"]]
-
         :repl-options {:init (set! *print-length* 100)}}}
+
+;;; uncomment and update versions to run a stand alone repl and using cider-connect
+;; {:repl {:plugins [[cider/cider-nrepl "0.14.0"]
+;;                   [refactor-nrepl "2.2.0"]]}}
