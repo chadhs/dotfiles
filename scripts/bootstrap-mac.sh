9c33439fca6f994290f6e79bd32b28eaa40651c0
Chad Stovern <chad@stovern.me>
2017-02-16 20:59:00 -0600

    mac bootstrap: removed app that is no longer available

--- scripts/bootstrap-mac.sh
+++ scripts/bootstrap-mac.sh
@@ -12,10 +12,10 @@ brew tap caskroom/cask
 
 echo "installing mac app store apps..."
 ## here are the app to id mappings for convenience:
-## 931657367 Calcbot, 918858936 Airmail, 496437906 Shush, 411643860 DaisyDisk, 407963104 Pixelmator, 872515009 Pomodoro Timer, 557168941 Tweetbot, 992076693 MindNode, 847496013 Deckset, 413965349 Soulver, 482898991 LiveReload, 924726344 Deliveries, 445189367 PopClip
+## 931657367 Calcbot, 918858936 Airmail, 496437906 Shush, 411643860 DaisyDisk, 407963104 Pixelmator, 557168941 Tweetbot, 992076693 MindNode, 847496013 Deckset, 413965349 Soulver, 482898991 LiveReload, 924726344 Deliveries, 445189367 PopClip
 read -p "enter your appleid email address: " appleid
 mas signin ${appleid}
-mas install 931657367 918858936 496437906 411643860 407963104 872515009 557168941 992076693 847496013 413965349 482898991 924726344 445189367
+mas install 931657367 918858936 496437906 411643860 407963104 557168941 992076693 847496013 413965349 482898991 924726344 445189367
 
 echo "installing open-source tools..."
 brew install ansible aspell awscli bash ctags editorconfig git leiningen mit-scheme nmap node pandoc planck rlwrap ssh-copy-id terraform the_silver_searcher tmux tree wget zsh
