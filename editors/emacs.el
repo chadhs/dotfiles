7850c3d3af99cb8a14e6da7034fd3fff852020f2
Chad Stovern <chad@stovern.me>
2017-06-08 09:49:16 -0500

    emacs testing:
    - added minor updates for toggling new use-package based test config
    - added a WIP use-package based emacs config

--- editors/emacs.el
+++ editors/emacs.el
@@ -3,8 +3,12 @@
 ;; prevent emacs from automatically adding a package section to this file
 ;(package-initialize)
 
+;; prevent emacs from saving customizations to this file
+(setq custom-file (concat user-emacs-directory ".emacs-customize.el"))
+
 ;; always follow the emacs-config.org symlink without prompting
 (setq vc-follow-symlinks t)
 
 ;; load the fully documented emacs-config.org configuration
 (org-babel-load-file "~/.emacs.d/emacs-config.org")
+;(org-babel-load-file "~/.emacs.d/emacs-config-up.org")
