df1f5d83eda418ed064a511bfa6e7cd81725e88f
Chad Stovern <chad@stovern.me>
2017-10-31 08:52:01 -0500

    misc updates:
    - emacs no emoji in yaml-mode
    - livereload aliases auto-background proc (quit on emacs term kill)
    - comment out extra lein plugins in profile

--- utils/lein_profiles.clj
+++ utils/lein_profiles.clj
@@ -1,9 +1,9 @@
 {:user {:plugins [
-                  [lein-ancient "0.6.10"]
+                  [lein-ancient "0.6.14"]
                   [lein-create-template "0.2.0"]
                   [lein-pprint "1.1.2"]
-                  [nightlight/lein-nightlight "1.7.2"]
-                  ;[walmartlabs/vizdeps "0.1.2"]
+                  ;; [nightlight/lein-nightlight "1.7.2"]
+                  ;; [walmartlabs/vizdeps "0.1.2"]
                   ]
         :repl-options {:init (set! *print-length* 100)}}}
 
