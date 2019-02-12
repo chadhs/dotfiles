5a02e40f491209b307f5eeeff303b36c26c69269
Chad Stovern <chad@stovern.me>
2019-02-12 16:56:55 -0600

    added version arg for install-lein.sh

--- utils/install-lein.sh
+++ utils/install-lein.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+lein_version=${1}
+
 cd ~/bin || exit 1
 rm lein
-wget https://raw.githubusercontent.com/technomancy/leiningen/2.8.3/bin/lein
+wget "https://raw.githubusercontent.com/technomancy/leiningen/${lein_version}/bin/lein"
 chmod +x lein
 ls -lah lein
 
