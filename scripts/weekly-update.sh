fb9abd1925b75bfcbbd44961943315cc49a536c4
Chad Stovern <chad@stovern.me>
2019-04-19 07:42:58 -0500

    add some safety checks to weekly-update.sh script

--- scripts/weekly-update.sh
+++ scripts/weekly-update.sh
@@ -1,10 +1,16 @@
 #!/usr/local/bin/zsh
 
+## preflight checks
+[ $(dirname "${0}") != "." ] && { echo "please run from within scripts dir, exiting..." ; exit 1; }
+[ ! -r npm-pkgs.txt ] || [ ! -r gem-pkgs.txt ] || [ ! -r pip-pkgs.txt ] && { echo "missing package lists, exiting..." ; exit 1; }
+
+## read in packages to install
 npm_pkgs="$(grep '^[^#[:blank:]]' npm-pkgs.txt | tr '\n' ' ')"
 gem_pkgs="$(grep '^[^#[:blank:]]' gem-pkgs.txt | tr '\n' ' ')"
 pip_pkgs="$(grep '^[^#[:blank:]]' pip-pkgs.txt | tr '\n' ' ')"
-# in the install commands below, wrapping echo is for array->string conversion
 
+## do it!
+### in the install commands below, wrapping echo is for array->string conversion
 . ~/.sh_aliases \
   && update-tech-notes && update-dotfiles \
   && brew update && brew doctor && brew upgrade; brew cleanup \
