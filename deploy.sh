786d33ada02d6cbe315b8d7eba7f405db1120540
Chad Stovern <chad@stovern.me>
2021-05-14 11:13:26 -0500

    add espanso config linking

--- deploy.sh
+++ deploy.sh
@@ -66,6 +66,7 @@ symlink_configs(){
     [ ! -e ~/Library/Application\ Support/Code/User/keybindings.json ] && ln -s ~/dotfiles/editors/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
     [ ! -e ~/.vscodevimrc ] && ln -s ~/dotfiles/editors/vscode/vscodevimrc ~/.vscodevimrc
     [ ! -e ~/.ideavimrc ] && ln -s ~/dotfiles/editors/ideavimrc .ideavimrc
+    [ ! -e ~/Library/Preferences/espanso/default.yml ] && ln -s ~/iCloudDrive/root/configs/espanso/default.yml ~/Library/Preferences/espanso/default.yml
   fi
 
 
