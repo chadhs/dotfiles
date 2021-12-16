f2808f7d97b6a43a00ece23625f3e7746c5e3c9b
Chad Stovern <chad@stovern.me>
2021-12-16 10:25:39 -0600

    add script just for npm install

---
+++ scripts/npm-update.sh
@@ -0,0 +1,13 @@
+#!/usr/local/bin/zsh
+
+## preflight checks
+[ $(dirname "${0}") != "." ] && { echo "please run from within scripts dir, exiting..." ; exit 1; }
+[ ! -r npm-pkgs.txt ] && { echo "missing package list, exiting..." ; exit 1; }
+
+## read in packages to install
+npm_pkgs="$(grep '^[^#[:blank:]]' npm-pkgs.txt | tr '\n' ' ')"
+
+## do it!
+### in the install commands below, wrapping echo is for array->string conversion
+. ~/.sh_aliases \
+  && npm install -g $(echo "${npm_pkgs}") \
